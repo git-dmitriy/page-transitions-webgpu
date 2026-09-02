313e80a87ef29230c866e32dd3131050fa6aedb6
git-dmitriy <dmitriy.shalberkin@gmail.com>
2026-09-02 14:14:57 +0300

    chore(footer): update github link

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -2,7 +2,8 @@
   <footer id="footer">
     <a href="https://dmitriy-shalberkin.ru/" target="_blank" rel="noopener noreferrer">dmitriy-shalberkin.ru</a>
     <div>
-      <a href="https://github.com/git-dmitriy" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://github.com/git-dmitriy/page-transitions-webgpu" target="_blank"
+         rel="noopener noreferrer">GitHub</a>
     </div>
   </footer>
 </template>
